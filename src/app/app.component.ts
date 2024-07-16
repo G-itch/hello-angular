a8532c2b57a8d17f35d05953f9087dfad3da7b10
Enzo Furtini <enzofurtini@tidsoftware.com.br>
2024-07-16 08:32:34 -0300

    Ngprime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MenubarModule } from 'primeng/menubar';
+import { Button, ButtonModule} from 'primeng/button';
+import {AvatarModule} from 'primeng/avatar';
+import {BadgeModule} from 'primeng/badge';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MenubarModule,ButtonModule,AvatarModule,BadgeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
